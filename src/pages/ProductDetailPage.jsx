33ee3521406afad2ce58bf8656ad622fe9590486
ashwini733 <ashwinirbhandari496@gmail.com>
2025-05-18 12:51:35 +0530

    Update ProductDetailPage.jsx

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -11,7 +11,7 @@ const ProductDetailPage = () => {
     const fetchProduct = async () => {
       try {
         const response = await fetch(
-          `http:///localhost:5000/api/products/${id}`
+          `${import.meta.env.VITE_API_URL}/api/products/${id}`
         );
         const data = await response.json();
         setProduct(data);
